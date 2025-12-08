8809c3fc69e16977c2ae506381d1db63b01dab2e
Manal Farouqi <farouqimanal@gmail.com>
2025-12-08 02:58:22 +0100

    Fix config for CI/CD

--- service-analyse/app/core/config.py
+++ service-analyse/app/core/config.py
@@ -4,22 +4,21 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent   
 
 class Settings(BaseSettings):
-    SK: str
-    ALG: str = "HS256"
+    # Variable ANALYSE
     HF_TOKEN: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-
-    DB_HOST: str 
-    DB_PORT: int 
-    DB_NAME: str 
-    DB_USER: str 
-    DB_PASSWORD: str 
     
-
-    model_config = {
-        "extra": "ignore",
-        "env_file": str(BASE_DIR / ".env"),  
-        "case_sensitive": True
-    }
+    # Variables optionnelles pour autres services
+    SK: str = "not_needed"
+    ALG: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    DB_HOST: str = "not_needed"
+    DB_PORT: int = 5432
+    DB_NAME: str = "not_needed"
+    DB_USER: str = "not_needed"
+    DB_PASSWORD: str = "not_needed"
+    GEMINI_API_KEY: str = "not_needed"
+    
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
